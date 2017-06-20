5df13e91f2c90343cc6e55c4cad6c11042e946f1
Ruben <ruben.sikkens@student.hu.nl>
2017-06-20 16:20:38 +0200

    Heroku Deployment options

--- dagboek/src/main/java/voedseldagboek/dagboek/persistence/BaseDAO.java
+++ dagboek/src/main/java/voedseldagboek/dagboek/persistence/BaseDAO.java
@@ -1,22 +1,44 @@
 package voedseldagboek.dagboek.persistence;
 
+import java.net.URI;
 import java.sql.Connection;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 public class BaseDAO {
-	protected final Connection getConnection() {
-		Connection result = null;
+	private DataSource connectionPool;
 
+	public BaseDAO() {
 		try {
-			InitialContext ic = new InitialContext();
-			DataSource datasource = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+			final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
+			if (DATABASE_URL_PROP != null) {
+				URI dbUri = new URI(DATABASE_URL_PROP);
+				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+				BasicDataSource pool = new BasicDataSource();
+				if (dbUri.getUserInfo() != null) {
+					pool.setUsername(dbUri.getUserInfo().split(":")[0]);
+					pool.setPassword(dbUri.getUserInfo().split(":")[1]);
+				}
+				pool.setDriverClassName("org.postgresql.Driver");
+				pool.setUrl(dbUrl);
+				pool.setInitialSize(1);
 
-			result = datasource.getConnection();
+				connectionPool = pool;
+			} else {
+				InitialContext ic = new InitialContext();
+				connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected final Connection getConnection() {
+		try {
+			return connectionPool.getConnection();
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
-
-		return result;
 	}
 }
